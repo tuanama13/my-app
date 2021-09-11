c934dd4dbdc495d9375dcd1057c86af655ea8a51
tuanama13 <tuanama7@gmail.com>
2021-09-11 14:09:24 +0700

    Phase 1

---
+++ src/todo-list/TodoList.js
@@ -0,0 +1,11 @@
+import './TodoList.css'
+
+const TodoList =  () => {
+  return (
+    <ul>
+      <li>Todo 1</li>
+      <li>Todo 2</li>
+      <li>Todo 3</li>
+    </ul>
+  )
+}
---
+++ src/todo/Todo.js
@@ -0,0 +1,9 @@
+import './Todo.css'
+
+const Todo  = () => {
+ return (
+   <div>
+     <h3>Todo List</h3>
+   </div>
+ ) 
+}
